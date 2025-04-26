a54fa3a927cd037a8c002b4db3b52959e1c46a60
Sharon <sharonsharon9048461583@gmail.com>
2025-04-26 14:02:58 +0530

    testing

--- src/pages/adminPages/AdminDashboard.jsx
+++ src/pages/adminPages/AdminDashboard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import SideBar from '../../components/SideBar'
+// import SideBar from '../../components/SideBar'
 
 function AdminDashboard() {
   return (
     <div>
-    <SideBar/>
+      eeerrrr
+    {/* <SideBar/> */}
     </div>
   )
 }
