4589dfadcce88109c50ce884fab51335623ab29d
Sharon <sharonsharon9048461583@gmail.com>
2025-06-16 17:16:56 +0530

    started host verification form

---
+++ src/components/HostComponets/BecomeHost/BecomeHostForm.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { requestedOtp, verifyHostOtp } from "../../../services/host/becomeHostServices";
+
+const BecomeHostForm = () => {
+    const [ formData , setFormData ] = useState({
+        name : "",
+        mobile: "",
+        panNumber : "",
+        panImage : "",
+    });
+
+    const [ otp , setOtp ] = useState("");
+    const [ otpSent , setOtpSent ] = useState(false);
+
+    const handleChange = (e) => {
+        const { name , value, files } = e.target;
+        setFormData((prev) => ({
+            ...prev,[name] : files ? files[0] : value,
+        }));
+    };
+
+    const handleRequestOtp = async (e) => {
+        e.preventDefault();
+
+        const result = await requestedOtp(formData);
+
+        if ( result.success ) {
+            toast.success("OTP sent to your mobile number");
+            setOtpSent(true);
+        } else {
+            toast.error("Failed to send OTP.Please try again!" || result.message );
+        }
+    };
+
+
+const handleVerifyOtp = async () => {
+    const result = await verifyHostOtp(formData.mobile, otp );
+
+    if ( result.success ) {
+        toast.success("OTP verified successfully! your host request being processed.");
+    } else {
+        toast.error(result.message || "Invalid OTP ");
+    }
+};
+
+return (
+    <div>
+        <h2>
+            Become a Host
+        </h2>
+        <form onSubmit={handleRequestOtp}>
+            <input type="text" name="name" placeholder="Name as per Pancard" value={formData.name} onChange={handleChange} required/>
+            <input type="text" name="mobile" placeholder="Enter Moblile Number connected with Pancard" value={formData.mobile} onChange={handleChange} required/>
+            <input type="text" name="PanNumber" placeholder="Enter Moblile Number connected with Pancard" value={formData.panNumber} onChange={handleChange} required/>
+            <input type="file" name="PanImage" accept="image/*" onChange={handleChange} required/>
+            <button type="submit"> Send OTP </button>
+            
+        </form>
+        {otpSent && (
+            <div>
+                <input type="text" placeholder="Enter OTP" value={otp} onChange={(e) => setOtp(e.target.value)} />
+                <button onClick={handleVerifyOtp}>
+                    Verify OTP & 
+                    Submit 
+                </button>
+            </div>
+        )}
+    </div>
+)
+
+};
+
+export default BecomeHostForm;
---
+++ src/services/host/becomeHostServices.js
@@ -0,0 +1,51 @@
+import api from "../../utils/api/api/";
+import uploadImageToCloudinary from "../../utils/uploadCloudinary/uploadCloudinary";
+import { toast } from "sonner";
+
+export const requestedOtp = async (formData ) => {
+    try {
+        const panImageUrl = await uploadImageToCloudinary(formData.panImage);
+        if ( !panImageUrl ){
+            toast.error("PAN image upload failed.Try again");
+            return { success : false, message : "PAN image upload failed"};
+        }
+        toast.success("Pan Image uploaded");
+
+        const response = await api.post("/api/host/auth/send-otp", {
+            name : formData.name,
+            mobile : formData.mobile,
+            panNumber : formData.PanNumber,
+            PanImage : panImageUrl,
+        });
+         if ( response.data.success) {
+            toast.success("An OTP send to your mobile.Please enter the OTP for verification");
+         }
+        return response.data;
+        
+    } catch ( error ) {
+        console.error("Request OTP Error:",error);
+        toast.error("Failed to send OTP.Try again");
+        return { success : false , message : "Failed to send OTP .please try again."};
+    };
+};
+
+export const verifyHostOtp = async (mobile, otp ) => {
+    try {
+        const response = await api.post("/host/auth/verify-otp", {
+            mobile,
+            otp,
+        });
+
+        if( response.data.success ) {
+            toast.success("OTP verified");
+        } else {
+            toast.error("invalid OTP");
+        }
+
+        return response.data;
+    } catch ( error ) {
+        console.error("Verify OTP Error:", error);
+        toast.error("Error verifying OTP");
+        return { success : false, message : "OTP verification failed"};
+    }; 
+};
