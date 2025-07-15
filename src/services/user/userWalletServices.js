268f6ef1f3881e9a19cf6b3546e37f1e320f0f66
Sharon <sharonsharon9048461583@gmail.com>
2025-07-15 09:12:01 +0530

    completed wallet implementations

---
+++ src/services/user/userWalletServices.js
@@ -0,0 +1,14 @@
+import api from "../../utils/api/api";
+
+export const fetchUserWallet = async () => {
+    try {
+        const response = await api.get("/user/wallet/info");
+       console.log( "ahfjkaldfhkl",+response.data)
+        return response;
+
+    } catch ( error ) {
+        throw new Error(error.response?.data?.message || "Failed to fetch use wllate info");
+    }
+    };
+
+
