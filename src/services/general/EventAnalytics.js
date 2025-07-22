eb422bf3429acfa2c594b46a3805710bdcb92ce9
Sharon <sharonsharon9048461583@gmail.com>
2025-07-22 21:20:57 +0530

    chnaged logic in service file like the event Analytical file and services files also

--- src/services/general/EventAnalytics.js
+++ src/services/general/EventAnalytics.js
@@ -10,3 +10,21 @@ export const fetchEvents = async ({ endpoint = "/admin/event-management/events",
     throw new Error("Failed to fetch events");
   }
 };
+
+
+
+
+// Fetch event summary analytics
+export const getEventBookingsSummary = async (eventId) => {
+  try {
+    const response = await api.get(`/event_analytics/${eventId}/summary`);
+    
+    return response.data;
+  } catch (error) {
+    console.error("Error while fetching host event booking summary:", error);
+    return {
+      success: false,
+      message: error.response?.data?.message || "Failed to load event analytics",
+    };
+  }
+};
