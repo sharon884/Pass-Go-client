b5a4634feee8a5c21aae6519c75d40f242ae5f42
Sharon <sharonsharon9048461583@gmail.com>
2025-07-29 17:25:53 +0530

    added new implemented infosection to landingpage

--- src/pages/generalPages/LandingPage.jsx
+++ src/pages/generalPages/LandingPage.jsx
@@ -3,12 +3,14 @@ import Navbar from '../../components/generalComponents/Navbar';
 import Footer from '../../components/generalComponents/Footer';
 import LandingComponent from '../../components/generalComponents/landing';
 import LandingBanner from '@/components/Landing/LandingBanner';
+import InfoSection from '@/components/Landing/InfoSection';
 function LandingPage() {
   return (
     <div>
        <Navbar/>
       <div>
       <LandingBanner/>
+      <InfoSection/>
       </div>
       <div>
 
