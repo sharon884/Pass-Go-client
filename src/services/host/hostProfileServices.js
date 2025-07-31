3db085a1ef47d3c22f8bde1114712fad9092f455
Sharon <sharonsharon9048461583@gmail.com>
2025-07-31 23:19:59 +0530

    fixed bug in the host edit profile component

--- src/services/host/hostProfileServices.js
+++ src/services/host/hostProfileServices.js
@@ -1,15 +1,15 @@
 import api from "../../utils/api/api";
 
-//Fetch Host Profile
-export const getHostProfile = async () => {
-  try {
-    const response = await api.get("/host/profile/get-host-Profile");
-    return response.data;
-  } catch (error) {
-    console.log("Error fetching useer profile", error);
-    throw new Error("Failed to load Host profile. Please try again later");
-  }
-};
+// //Fetch Host Profile
+// export const getHostProfile = async () => {
+//   try {
+//     const response = await api.get("/host/profile/get-host-Profile");
+//     return response.data;
+//   } catch (error) {
+//     console.log("Error fetching useer profile", error);
+//     throw new Error("Failed to load Host profile. Please try again later");
+//   }
+// };
 
 //Fetch Host Profile
 export const updateHostProfile = async (data) => {
