0b98882e1917ff48cd8115152b69d846702a86bb
Sharon <sharonsharon9048461583@gmail.com>
2025-07-31 23:33:21 +0530

    removed the unused verify request component from the host profile component

--- src/components/HostComponets/HostProfileComponet.jsx
+++ src/components/HostComponets/HostProfileComponet.jsx
@@ -450,7 +450,7 @@ const HostProfile = () => {
                     Change Password
                   </button>
                   <div className="flex items-center justify-center">
-                    <VerifyRequestButton />
+                    {/* <VerifyRequestButton /> */}
                   </div>
                 </div>
               </div>
