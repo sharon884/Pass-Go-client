0af6718cab98fc036b38812e91c96383be1dbfd3
Sharon <sharonsharon9048461583@gmail.com>
2025-08-10 16:00:34 +0530

    updated the landingpage component

--- src/pages/generalPages/LandingPage.jsx
+++ src/pages/generalPages/LandingPage.jsx
@@ -5,12 +5,14 @@ import LandingComponent from '../../components/generalComponents/Landing';
 import LandingBanner from '@/components/Landing/LandingBanner';
 import InfoSection from '@/components/Landing/InfoSection';
 import RunningEvents from '@/components/Landing/RunningEvents';
+import PassGoStrip from '@/components/Landing/PassGoStrip';
 function LandingPage() {
   return (
     <div>
        <Navbar/>
       <div>
       <LandingBanner/>
+      <PassGoStrip/>
       <InfoSection/>
       <RunningEvents/>
       </div>
