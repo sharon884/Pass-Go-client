62ab6b43b72d115dd661025c51017bac751affce
Sharon <sharonsharon9048461583@gmail.com>
2025-09-26 01:02:53 +0530

    removed the breducrumb compoent

--- src/components/ui/Breadcrumb/BreadcrumbUpdater.jsx
+++
@@ -1,9 +0,0 @@
-
-import useBreadcrumbGenerator from "../../../hooks/useBreadcrumbGenerator";
-
-const BreadcrumbUpdater = () => {
-  useBreadcrumbGenerator();
-  return null; 
-};
-
-export default BreadcrumbUpdater;
