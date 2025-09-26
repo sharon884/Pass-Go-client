d5f5b35e4b14a6069fe2d89c4171a39ce92b41e4
Sharon <sharonsharon9048461583@gmail.com>
2025-09-26 10:25:06 +0530

    updated the admimin cancellation page

--- src/pages/adminPages/AdminCancelRequestsPage.jsx
+++ src/pages/adminPages/AdminCancelRequestsPage.jsx
@@ -1,19 +1,32 @@
+import React from 'react'
 import AdminCancellationRequests from '@/components/AdminComponents/EventManagement/AdminCancellationRequests'
 import AdminNavbar from '@/components/AdminComponents/Navbar'
 import AdminSidebar from '@/components/generalComponents/SideBar'
-import React from 'react'
-import { Footer } from 'react-day-picker'
+import Footer from '../../components/generalComponents/Footer' // Corrected import path
 
 function AdminCancelRequestsPage() {
-  return (
-    <div>
-        <AdminNavbar/>
-        <AdminSidebar/>
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-slate-900">
+      
+      {/* 1. The main Navbar at the top of the page */}
+      <AdminNavbar />
 
-      <AdminCancellationRequests/>
-      <Footer/>
-    </div>
-  )
+      {/* 2. The main layout container for the sidebar and content */}
+      <div className="flex flex-1">
+        
+        {/* Sidebar - fixed on the left */}
+        <AdminSidebar />
+
+        {/* Main Content Area - takes the remaining space */}
+        <main className="flex-1 p-4 md:p-6 lg:p-8 overflow-y-auto">
+          <AdminCancellationRequests />
+        </main>
+      </div>
+
+      {/* 3. Footer at the bottom */}
+      <Footer />
+    </div>
+  )
 }
 
 export default AdminCancelRequestsPage
